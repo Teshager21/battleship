979e309c4b7473998475108a42faaf87112058c9
teshager <teshager8922@gmail.com>
2024-08-19 12:43:38 +0300

    add a failing test for isSunk method of battleship

--- BattleShip.test.js
+++ BattleShip.test.js
@@ -1,7 +1,15 @@
 import BattleShip from "./BattleShip";
-it('should take a hit',()=>{
-    const battleShip= new BattleShip();
+it('should return a received_hit of one',()=>{
+    const battleShip= new BattleShip(3);
     battleShip.hit();
     expect(battleShip.recieved_hit).toBe(1);
 })
+
+it(('it should return sunk-true',()=>{
+    const battleShip = new BattleShip(1);
+    battleShip.hit();
+    expect(battleShip.isSunk()).toBe(true);
+}))
+
+
 
