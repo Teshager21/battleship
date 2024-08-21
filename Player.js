978340a831685df35198a0056f9fa4f217698bda
teshager <teshager8922@gmail.com>
2024-08-21 08:28:18 +0300

    add a deploy fleet method to the player class

--- Player.js
+++ Player.js
@@ -1,6 +1,9 @@
-class Player{
+export default class Player{
     constructor(gameBoard){
         this.gameBoard=gameBoard;
 
+    }
+    deployFleet(){
+        this.gameBoard.placeShips();
     }
 }
