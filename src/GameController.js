1be4a8d5b46d4a033068e0528b35255d44050392
teshager <teshager8922@gmail.com>
2024-08-22 12:33:41 +0300

    switch turns between players

--- src/GameController.js
+++ src/GameController.js
@@ -1,6 +1,7 @@
 import GameBoard from "./GameBoard.js";
 import Player from "./Player.js";
 import View from './GameView';
+import subscription from "./EventEmitter.js";
 const Controller=()=>{
 
     const gameBoard1= new GameBoard();
@@ -10,16 +11,32 @@ const Controller=()=>{
     
     player1.deployFleet();
     player2.deployFleet();
-    
+    const view=View();
+
+    let currentPlayer= player1;
+    let opponent= player2;
+    const switchTurn=()=>{
+        if(currentPlayer===player1) {
+            currentPlayer=player2;
+            opponent=player1;
+        }else{
+            currentPlayer=player1;
+            opponent=player2;
+            }
+    }
     const init=()=>{
-        const view=View();
-        player1.gameBoard.receiveAttack('20');
-        player1.gameBoard.receiveAttack('22');
-        player2.gameBoard.receiveAttack('20');
-        player2.gameBoard.receiveAttack('22');
-        // console.log(gameBoard1.fleet);
+        currentPlayer.gameBoard.receiveAttack('20');
+        currentPlayer.gameBoard.receiveAttack('22');
+        console.log(gameBoard1.fleet);
         view.display(gameBoard1,gameBoard2);
     }
+    const allocateFire=(location)=>{
+      opponent.gameBoard.receiveAttack(location);
+      view.display(gameBoard1,gameBoard2);
+      switchTurn();
+    }
+    subscription.publish('fire',allocateFire)
+
    return {init}
 }
 
